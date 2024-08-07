056454320c713760299ab9df848c3a09e238d605
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2024-08-07 18:35:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
   currFolder = folder;
   //console.log(currFolder);
-  let a = await fetch(`../songs/${currFolder}`);
+  let a = await fetch(`VBeatZ-Music-app-/Songs/${currFolder}`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -88,19 +88,19 @@ async function getSongs(folder) {
 
 // to play music
 async function playMusic(track) {
-  currentSong.src = `../songs/${currFolder}/` + track;
+  currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + track;
   currentSong.play();
   document.querySelector(".songInfo").innerHTML = track
     .split("-")[0]
     .replaceAll("%20", " ");
-  play.src = "../images/pause-stroke-rounded.svg";
+  play.src = "VBeatZ-Music-app-/images/pause-stroke-rounded.svg";
 }
 
 // to display all the folders as a card on screen
 
 async function displayAlbums() {
   console.log("albums");
-  let a = await fetch(`../songs/`);
+  let a = await fetch(`VBeatZ-Music-app-/Songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -113,13 +113,13 @@ async function displayAlbums() {
     if (element.href.includes("/songs/")) {
       folder = element.href.split("/").slice(-1)[0];
       //console.log(folder);
-      let a = await fetch(`../songs/${folder}/description.json`);
+      let a = await fetch(`VBeatZ-Music-app-/Songs/${folder}/description.json`);
       let response = await a.json();
       //console.log(Object.keys(response));
       cardContainer.innerHTML =
         cardContainer.innerHTML +
         `<div data-set=${folder} class="card black">
-                <img src="../songs/${folder}/cover.jpg" alt="">
+                <img src="VBeatZ-Music-app-/Songs/${folder}/cover.jpg" alt="">
                 <h2>${response.title}</h2>
                 <p>${response.description}</p>
            </div>`;
@@ -166,10 +166,10 @@ async function main() {
   play.addEventListener("click", () => {
     if (currentSong.paused) {
       currentSong.play();
-      play.src = "../images/pause-stroke-rounded.svg";
+      play.src = "VBeatZ-Music-app-/images/pause-stroke-rounded.svg";
     } else {
       currentSong.pause();
-      play.src = "../images/play-circle-stroke-rounded.svg";
+      play.src = "VBeatZ-Music-app-/images/play-circle-stroke-rounded.svg";
     }
   });
 
@@ -182,7 +182,7 @@ async function main() {
     console.log(`current song index= ${index}`);
     if (index === 0) {
       console.log(`current song index= ${songs.length - 1}`);
-      currentSong.src = `../songs/${currFolder}/` + songs[songs.length - 1];
+      currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + songs[songs.length - 1];
       playMusic(currentSong.src.split(`/${currFolder}/`)[1]);
     } else {
       let newIndex = index - 1;
@@ -199,12 +199,12 @@ async function main() {
     //console.log(songs.length);
     if (index + 1 === songs.length) {
       console.log(`current song index= ${0}`);
-      currentSong.src = `../songs/${currFolder}/` + songs[0];
+      currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + songs[0];
       playMusic(currentSong.src.split(`/${currFolder}/`)[1]);
     } else {
       let newIndex = index + 1;
       console.log(`new song index ${newIndex}`);
-      currentSong.src = `../songs/${currFolder}/` + songs[newIndex];
+      currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + songs[newIndex];
       playMusic(currentSong.src.split(`/${currFolder}/`)[1]);
     }
   });
@@ -224,12 +224,12 @@ async function main() {
       //console.log(songs.length);
       if (index + 1 === songs.length) {
         //console.log(0);
-        currentSong.src = `../songs/${currFolder}/` + songs[0];
+        currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + songs[0];
         playMusic(currentSong.src.split(`/${currFolder}/`)[1]);
       } else {
         let newIndex = index + 1;
         console.log(`new song index ${newIndex}`);
-        currentSong.src = `../songs/${currFolder}/` + songs[newIndex];
+        currentSong.src = `VBeatZ-Music-app-/Songs/${currFolder}/` + songs[newIndex];
         playMusic(currentSong.src.split(`/${currFolder}/`)[1]);
       }
     }
